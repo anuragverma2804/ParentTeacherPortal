ff8d75b176870bdb2c8f9ce984772fc64811d89e
anurag_verma <anuragverma228497@gmail.com>
2025-05-01 19:06:21 +0530

    Reformated File

--- parentTeacherPortal/parentTeacherApp/views.py
+++ parentTeacherPortal/parentTeacherApp/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .models import Teacher_profile, Parent_profile
-# Create your views here.
+from .models import TeacherProfile, StudentProfile, SchoolProfile
+
 
 def home(request):
     return render(request, 'index.html')
@@ -12,16 +12,16 @@ def home(request):
 
 def login_user(request):
     if request.method == 'POST':
-        role=request.POST.get('role')
+        role = request.POST.get('role')
         email = request.POST.get('email')
         passw = request.POST.get('pass')
         username = email.split('@')[0]
         user = authenticate(username=username, password=passw)
-        profile=None
+        profile = None
         if user is not None:
             if (role == "teacher"):
                 try:
-                    profile=Teacher_profile.objects.get(email=email)
+                    profile = TeacherProfile.objects.get(email=email)
                 except ObjectDoesNotExist:
                     messages.error(request, "Incorrect Role")
                 if profile is not None:
@@ -29,9 +29,8 @@ def login_user(request):
                     return render(request, 'teacher_profile.html')
             elif (role == "parent"):
                 try:
-                    profile=Parent_profile.objects.get(email=email)
+                    profile = StudentProfile.objects.get(email=email)
                 except ObjectDoesNotExist:
-                    print(user)
                     messages.error(request, "Incorrect Role")
                 if profile is not None:
                     login(request, user)
@@ -42,29 +41,28 @@ def login_user(request):
     return render(request, 'login.html')
 
 
-
 def signup(request):
     if request.method == 'POST':
-        role=request.POST.get('role')
-        fname=request.POST.get('fname')
-        lname=request.POST.get('lname')
-        email=request.POST.get('email')
-        number=request.POST.get('number')
-        pass1=request.POST.get('pass1')
-        pass2=request.POST.get('pass2')
-        username=email.split('@')[0]
-        if pass1==pass2:
+        role = request.POST.get('role')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        username = email.split('@')[0]
+        if pass1 == pass2:
             myuser = User.objects.create_user(username=username, email=email, password=pass1)
             myuser.save()
-            if role=='teacher':
-                profile = Teacher_profile(user=User.objects.get(username=username),
-                                   first_name=fname, last_name=lname, phone=number,email=email)
+            if role == 'teacher':
+                profile = TeacherProfile(user=User.objects.get(username=username),
+                                         first_name=fname, last_name=lname, phone=number, email=email)
                 profile.save()
                 return render(request, 'login.html')
 
-            elif role=='parent':
-                profile = Parent_profile(user=User.objects.get(username=username),
-                                   first_name=fname, last_name=lname, phone=number,email=email)
+            elif role == 'parent':
+                profile = StudentProfile(user=User.objects.get(username=username),
+                                         first_name=fname, last_name=lname, phone=number, email=email)
                 profile.save()
                 return render(request, 'login.html')
             else:
